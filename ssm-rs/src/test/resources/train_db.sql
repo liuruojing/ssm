b0d46c6c02fac71c3a7ff1cc1057b6b7cf4d0c07
liuruojing <liuruojing@richstonedt.com>
2018-07-26 09:17:15 +0800

    docs: 更新数据库train_db.sql文件

--- ssm-rs/src/test/resources/train_db.sql
+++ ssm-rs/src/test/resources/train_db.sql
@@ -1,7 +1,3 @@
--- noinspection SqlDialectInspectionForFile
-
--- noinspection SqlNoDataSourceInspectionForFile
-
 /*
 Navicat MySQL Data Transfer
 
@@ -14,18 +10,132 @@ Target Server Type    : MYSQL
 Target Server Version : 50722
 File Encoding         : 65001
 
-Date: 2018-07-11 23:35:14
+Date: 2018-07-26 09:12:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for tra_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `tra_permission`;
+CREATE TABLE `tra_permission` (
+  `permission_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `permission_name` varchar(255) NOT NULL,
+  `perimission_url` varchar(255) NOT NULL,
+  `permissioon_type` bigint(20) NOT NULL,
+  PRIMARY KEY (`permission_id`),
+  KEY `FK_permissionType` (`permissioon_type`),
+  CONSTRAINT `FK_permissionType` FOREIGN KEY (`permissioon_type`) REFERENCES `tra_permissiontype` (`permissonType_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tra_permission
+-- ----------------------------
+INSERT INTO `tra_permission` VALUES ('1', 'prize:add', '/v1.0/prize', '1');
+
+-- ----------------------------
+-- Table structure for tra_permissiontype
+-- ----------------------------
+DROP TABLE IF EXISTS `tra_permissiontype`;
+CREATE TABLE `tra_permissiontype` (
+  `permissonType_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `permissionType_name` varchar(255) NOT NULL,
+  KEY `permissonType_id` (`permissonType_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tra_permissiontype
+-- ----------------------------
+INSERT INTO `tra_permissiontype` VALUES ('1', '奖品管理');
 
 -- ----------------------------
 -- Table structure for tra_prizes
 -- ----------------------------
 DROP TABLE IF EXISTS `tra_prizes`;
 CREATE TABLE `tra_prizes` (
-  `prize_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `prize_name` varchar(50) NOT NULL,
-  `created_time` datetime NOT NULL,
+  `prize_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '唯一表识',
+  `prize_name` varchar(50) NOT NULL COMMENT '奖品名称',
+  `created_time` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`prize_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of tra_prizes
+-- ----------------------------
+INSERT INTO `tra_prizes` VALUES ('1', 'string', '2018-07-25 11:55:18');
+INSERT INTO `tra_prizes` VALUES ('2', 'string', '2018-07-25 14:54:39');
+
+-- ----------------------------
+-- Table structure for tra_role
+-- ----------------------------
+DROP TABLE IF EXISTS `tra_role`;
+CREATE TABLE `tra_role` (
+  `role_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `uniq_rolename` (`role_name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tra_role
+-- ----------------------------
+INSERT INTO `tra_role` VALUES ('1', 'admin');
+
+-- ----------------------------
+-- Table structure for tra_role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `tra_role_permission`;
+CREATE TABLE `tra_role_permission` (
+  `role_permission_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`role_permission_id`),
+  UNIQUE KEY `uniq_roleId_permissionId` (`role_id`,`permission_id`) USING BTREE,
+  KEY `FK_permission_id` (`permission_id`),
+  CONSTRAINT `FK_permission_id` FOREIGN KEY (`permission_id`) REFERENCES `tra_permission` (`permission_id`),
+  CONSTRAINT `FK_role_ids` FOREIGN KEY (`role_id`) REFERENCES `tra_role` (`role_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tra_role_permission
+-- ----------------------------
+INSERT INTO `tra_role_permission` VALUES ('1', '1', '1');
+
+-- ----------------------------
+-- Table structure for tra_user
+-- ----------------------------
+DROP TABLE IF EXISTS `tra_user`;
+CREATE TABLE `tra_user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) NOT NULL,
+  `user_password` varchar(15) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `uniq_username` (`user_name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tra_user
+-- ----------------------------
+INSERT INTO `tra_user` VALUES ('1', 'liuruojing', '123456');
+
+-- ----------------------------
+-- Table structure for tra_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `tra_user_role`;
+CREATE TABLE `tra_user_role` (
+  `user_role_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_role_id`),
+  UNIQUE KEY `uniq_userId_roleId` (`user_id`,`role_id`) USING BTREE,
+  KEY `FK_user_id` (`user_id`) USING BTREE,
+  KEY `FK_role_id` (`role_id`),
+  CONSTRAINT `FK_role_id` FOREIGN KEY (`role_id`) REFERENCES `tra_role` (`role_id`),
+  CONSTRAINT `FK_user_id` FOREIGN KEY (`user_id`) REFERENCES `tra_user` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of tra_user_role
+-- ----------------------------
+INSERT INTO `tra_user_role` VALUES ('1', '1', '1');
